04032331395875a4b878e385212aec7cf9da46d2
shoeone96 <dlwnddnjs96@gmail.com>
2021-08-15 02:40:30 +0900

    자바 ORM 표준 JPA 프로그래밍
    N:M 관계
    쓰지마

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -22,9 +24,13 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
     public Long getId() {
         return id;
     }
+    @ManyToMany
+    @JoinTable(name = "MEMBER_PRODUCT")
+    private List<Product> products = new ArrayList<>();
 
     public void setId(Long id) {
         this.id = id;
